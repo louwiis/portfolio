78c8bfbc250c21e7e6f9982e63f083a8b46f225d
Luis Teixeira Lima <dev.louwis@gmail.com>
2025-06-16 17:48:23 +0200

    edit

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: process.env.NODE_ENV === "production" ? "/" : "/",
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
